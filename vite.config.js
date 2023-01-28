fc1bc6aa6fadd41c20463ec087a1e4b413b4c05c
姚旭 <xu.yao@zeropartner.com>
2023-01-28 11:16:27 +0800

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,43 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import ViteRequireContext from "@originjs/vite-plugin-require-context";
+import requireTransform from "vite-plugin-require-transform";
+import AutoImport from "unplugin-auto-import/vite";
+import viteCompression from "vite-plugin-compression";
+import vueSetupExtend from "vite-plugin-vue-setup-extend";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    // 支持require.context
+    ViteRequireContext(),
+    // 支持使用require
+    requireTransform({}),
+
+    AutoImport({
+      // 可直接使用vue、router中的composition api，无需再import
+      imports: ["vue", "vue-router"],
+    }),
+
+    // 压缩资源
+    viteCompression(),
+
+    // setup语法糖name增强，使vue3语法糖支持name属性。 <script setup name="Home">
+    // vue3语法糖默认是没有name属性的，在我们使用keep-alive的时候需要用到name
+    vueSetupExtend(),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 8899,
+    open: true,
+  },
+});
